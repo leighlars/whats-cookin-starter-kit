3bb94876e4bf629ce1c81e1330c749be400f4b27
Leigh Larson <leighlarson4@gmail.com>
2020-07-07 13:53:40 -0600

    Add addFavoriteRecipe() and deleteFavoriteRecipe()

--- src/User.js
+++ src/User.js
@@ -1,3 +1,5 @@
+// const recipesData = require('../data/recipe');
+
 class User {
   constructor(user) {
     this.id = user.id;
@@ -6,6 +8,16 @@ class User {
     this.favoriteRecipes = user.favoriteRecipes || [];
     this.plannedRecipes = user.plannedRecipes || [];
   }
+
+  addFavoriteRecipe(recipe) {
+    this.favoriteRecipes.push(recipe);
+  }
+
+  deleteFavoriteRecipe(recipe) {
+    let i = this.favoriteRecipes.indexOf(recipe);
+    this.favoriteRecipes.splice(i, 1);
+  }
+
 }
 
 
--- test/user-test.js
+++ test/user-test.js
@@ -1,10 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
 const User = require('../src/User');
+const Recipe = require('../src/Recipe');
 
 describe('User', function() {
   let user;
   let userInfo;
+  let recipeInfo;
+  let recipe;
   beforeEach(function() {
     userInfo = {
       "name": "Saige O'Kon",
@@ -157,6 +160,69 @@ describe('User', function() {
       ]
     },
     user = new User(userInfo);
+    recipeInfo = {
+      "id": 595736,
+      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+      "ingredients": [
+        {
+          "id": 20081,
+          "quantity": {
+            "amount": 1.5,
+            "unit": "c"
+          }
+        },
+        {
+          "id": 18372,
+          "quantity": {
+            "amount": 0.5,
+            "unit": "tsp"
+          }
+        },
+        {
+          "id": 1123,
+          "quantity": {
+            "amount": 1,
+            "unit": "large"
+          }
+        }
+      ],
+      "instructions": [
+        {
+          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+          "number": 1
+        },
+        {
+          "instruction": "Add egg and vanilla and mix until combined.",
+          "number": 2
+        },
+        {
+          "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+          "number": 3
+        },
+        {
+          "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
+          "number": 4
+        },
+        {
+          "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
+          "number": 5
+        },
+        {
+          "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
+          "number": 6
+        }
+      ],
+      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+      "tags": [
+        "antipasti",
+        "starter",
+        "snack",
+        "appetizer",
+        "antipasto",
+        "hor d'oeuvre"
+      ]
+    },
+    recipe = new Recipe(recipeInfo);
   });
 
   it('should be a function', function() {
@@ -190,9 +256,16 @@ describe('User', function() {
     expect(user.plannedRecipes).to.deep.equal([]);
   });
 
-  // it('should have a name', function () {
-  //   expect(user.name).to.equal("Saige O'Kon");
-  // });
+  it('should be able to favorite recipes', function () {
+    user.addFavoriteRecipe(recipe);
+
+    expect(user.favoriteRecipes.length).to.deep.equal(1);
+  });
+
+  it('should delete recipe from favorites', function() {
+    user.deleteFavoriteRecipe(recipe);
+    expect(user.favoriteRecipes.length).to.deep.equal(0);
+  });
 
   
 
