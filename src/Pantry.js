6719910a7fe9ebb1ea6a35b4bf4875343a81a164
Leigh Larson <leighlarson4@gmail.com>
2020-07-07 19:33:47 -0600

    Add notes to method

--- src/Pantry.js
+++ src/Pantry.js
@@ -3,6 +3,7 @@ class Pantry {
   constructor(user) {
     this.pantry = user.pantry;
   }
+  // Determine if user has enough ingredients in their pantry (and the amount of ingredients still needed to cook a given meal), based on what’s in my pantry
   
   determineSufficientIngredients(recipe) {
     const notEnoughIngredients = this.pantry.reduce((acc, pantryIngredient) => {
@@ -18,11 +19,9 @@ class Pantry {
       }
       return acc;
     }, []);
-    // console.log(notEnoughIngredients);
     return notEnoughIngredients.length === 0 ? true : notEnoughIngredients;
   }
 }
-// Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
     
 // Remove the ingredients used for a given meal from my pantry, once that meal has been cooked(only applicable if users have a list of mealsToCook; can be considered a stretch goal)
 
