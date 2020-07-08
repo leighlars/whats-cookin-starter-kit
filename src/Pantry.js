12d1b94978bf4b8f8575bf9e202236e58b55a804
Leigh Larson <leighlarson4@gmail.com>
2020-07-07 19:17:00 -0600

    Add determineSufficientAmount method, but didn't pass test

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,6 +1,29 @@
-// Determine whether my pantry has enough ingredients to cook a given meal
+
+class Pantry {
+  constructor(user) {
+    this.pantry = user.pantry;
+  }
+  
+  determineSufficientIngredients(recipe) {
+    const notEnoughIngredients = this.pantry.reduce((acc, pantryIngredient) => {
+      const recipeIngredientToCompare = recipe.ingredients.find(recipeIngredient => {
+        return pantryIngredient.ingredient === recipeIngredient.id
+      })
+      if (recipeIngredientToCompare) {
+        if (recipeIngredientToCompare.quantity.amount > pantryIngredient.amount) {
+          recipeIngredientToCompare.quantity.amount = recipeIngredientToCompare.quantity.amount - pantryIngredient.amount
+          acc.push(recipeIngredientToCompare);
+        }
+      }
+      return acc
+    }, [])
+    return notEnoughIngredients.length > 0 ? notEnoughIngredients : false;
+  }
+}
 // Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
+    
 // Remove the ingredients used for a given meal from my pantry, once that meal has been cooked(only applicable if users have a list of mealsToCook; can be considered a stretch goal)
+
 
 if (typeof module !== 'undefined') {
   module.exports = Pantry;
