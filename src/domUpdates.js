ea174f28948c97a8dbd0c2d162690e455d60c221
Leigh Larson <leighlarson4@gmail.com>
2020-07-08 16:52:06 -0600

    Add styling to modal, add code to domUpdates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -18,7 +18,42 @@ const domUpdates {
     allRecipeInfo.insertAdjacentHTML("beforebegin", "<section id='overlay'></section>");
 },
 
- 
+generateRecipeTitle(recipe, ingredients) {
+  let fullRecipeInfo = document.querySelector(".recipe-instructions");
+  let recipeTitle = `
+        <button id="exit-recipe-btn">X</button>
+        <h3 id="recipe-title">${recipe.name}</h3>
+        <h4>Ingredients</h4>
+        <p>${ingredients}</p>`
+  fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
+},
+
+  addRecipeImage(recipe) {
+    document.getElementById("recipe-title").style.backgroundImage = `url(${recipe.image})`;
+  },
+
+  generateInstructions(recipe) {
+    let allRecipeInfo = document.querySelector(".recipe-instructions");
+    let instructionsList = "";
+    let instructions = recipe.instructions.map(i => {
+      return i.instruction
+    });
+    instructions.forEach(i => {
+      instructionsList += `<li>${i}</li>`
+    });
+    allRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
+    allRecipeInfo.insertAdjacentHTML("beforeend", `<ol class="instructions">${instructionsList}</ol>`);
+  },
+
+  generateRecipeBtns(recipe) {
+    let allRecipeInfo = document.querySelector(".recipe-instructions");
+    let recipeButtons = `
+        <button class="cook-recipe" disabled id="${recipe.id}">Cook This Recipe</button>
+        <button class="calculate-cost" id="${recipe.id}">Cost to Cook</button>
+        <button class="check-pantry" id="${recipe.id}">Check Pantry</button>
+        `;
+    allRecipeInfo.insertAdjacentHTML("beforeend", recipeButtons);
+  }, 
 
 
 
