7d99d8e8c5c11ec4f3307473ecb0f36675acf4f6
Leigh Larson <leighlarson4@gmail.com>
2020-07-07 22:08:56 -0600

    Add tests to method shells in pantry

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,27 +1,53 @@
+const ingredientsData = require("../data/ingredients");
 
 class Pantry {
-  constructor(user) {
-    this.pantry = user.pantry;
+  constructor(ingredients = []) {
+    this.ingredients = ingredients;
   }
-  // Determine if user has enough ingredients in their pantry (and the amount of ingredients still needed to cook a given meal), based on what’s in my pantry
+
+  // Determine whether my pantry has enough ingredients to cook a given meal
   
-  determineSufficientIngredients(recipe) {
-    const notEnoughIngredients = this.pantry.reduce((acc, pantryIngredient) => {
-      const recipeIngredientToCompare = recipe.ingredients.find(recipeIngredient => {
-        return pantryIngredient.ingredient === recipeIngredient.id
-      });
-      console.log(recipeIngredientToCompare.quantity);
-      if (recipeIngredientToCompare) {
-        if (recipeIngredientToCompare.quantity.amount > pantryIngredient.amount) {
-          recipeIngredientToCompare.quantity.amount = recipeIngredientToCompare.quantity.amount - pantryIngredient.amount
-          acc.push(recipeIngredientToCompare);
-        }
-      }
-      return acc;
-    }, []);
-    return notEnoughIngredients.length === 0 ? true : notEnoughIngredients;
+  helperMethod = (recipeIngredient) => {
+    return this.ingredients.find(pantryIngredient => {
+      return pantryIngredient.ingredient === recipeIngredient.id &&
+        recipeIngredient.quantity.amount <= pantryIngredient.amount;
+    });
   }
-}
+
+  checkPantry = (recipe) => {
+    return recipe.ingredients.every(recipeIngredient => {
+      return this.helperMethod(recipeIngredient);
+    });
+  }
+
+ };
+  
+  // Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
+
+  // findMissingIngredients() {
+
+  // }
+
+
+  // determineSufficientIngredients(recipe) {
+  //   const notEnoughIngredients = this.pantry.reduce((acc, pantryIngredient) => {
+  //     const recipeIngredientToCompare = recipe.ingredients.find(recipeIngredient => {
+  //       return pantryIngredient.ingredient === recipeIngredient.id
+  //     });
+  //     console.log(recipeIngredientToCompare.quantity);
+  //     if (recipeIngredientToCompare) {
+  //       if (recipeIngredientToCompare.quantity.amount > pantryIngredient.amount) {
+  //         recipeIngredientToCompare.quantity.amount = recipeIngredientToCompare.quantity.amount - pantryIngredient.amount
+  //         acc.push(recipeIngredientToCompare);
+  //       }
+  //     }
+  //     return acc;
+  //   }, []);
+  //   return notEnoughIngredients.length === 0 ? true : notEnoughIngredients;
+  // }
+
+
+
     
 // Remove the ingredients used for a given meal from my pantry, once that meal has been cooked(only applicable if users have a list of mealsToCook; can be considered a stretch goal)
 
--- test/pantry-test.js
+++ test/pantry-test.js
@@ -1,171 +1,31 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Pantry = require('../src/Pantry');
-const User = require('../src/User');
+// const User = require('../src/User');
 const Recipe = require('../src/Recipe');
+// const ingredientsData = require("../data/ingredients");
+
 
 describe('Pantry', function() {
-  let pantry;
-  let user;
-  let userInfo;
-  let recipe1;
-  let recipeInfo1;
-  let recipe2;
-  let recipeInfo2;
+  let pantryFull, pantryEmpty, ingredients, recipe1, recipeInfo1, recipe2, recipeInfo2;
 
   beforeEach(function() {
-    userInfo = {
-      "name": "Saige O'Kon",
-      "id": 1,
-      "pantry": [
-        {
-          "ingredient": 11477,
-          "amount": 4
-        },
-        {
-          "ingredient": 11297,
-          "amount": 4
-        },
-        {
-          "ingredient": 1082047,
-          "amount": 10
-        },
-        {
-          "ingredient": 20081,
-          "amount": 5
-        },
-        {
-          "ingredient": 11215,
-          "amount": 5
-        },
-        {
-          "ingredient": 2047,
-          "amount": 6
-        },
-        {
-          "ingredient": 1123,
-          "amount": 8
-        },
-        {
-          "ingredient": 11282,
-          "amount": 4
-        },
-        {
-          "ingredient": 6172,
-          "amount": 2
-        },
-        {
-          "ingredient": 2044,
-          "amount": 2
-        },
-        {
-          "ingredient": 2050,
-          "amount": 4
-        },
-        {
-          "ingredient": 1032009,
-          "amount": 3
-        },
-        {
-          "ingredient": 5114,
-          "amount": 3
-        },
-        {
-          "ingredient": 1017,
-          "amount": 2
-        },
-        {
-          "ingredient": 18371,
-          "amount": 7
-        },
-        {
-          "ingredient": 1001,
-          "amount": 6
-        },
-        {
-          "ingredient": 99223,
-          "amount": 2
-        },
-        {
-          "ingredient": 1230,
-          "amount": 2
-        },
-        {
-          "ingredient": 9152,
-          "amount": 4
-        },
-        {
-          "ingredient": 10611282,
-          "amount": 2
-        },
-        {
-          "ingredient": 93607,
-          "amount": 2
-        },
-        {
-          "ingredient": 14106,
-          "amount": 4
-        },
-        {
-          "ingredient": 1077,
-          "amount": 4
-        },
-        {
-          "ingredient": 6150,
-          "amount": 2
-        },
-        {
-          "ingredient": 1124,
-          "amount": 2
-        },
-        {
-          "ingredient": 10011693,
-          "amount": 4
-        },
-        {
-          "ingredient": 1102047,
-          "amount": 2
-        },
-        {
-          "ingredient": 19206,
-          "amount": 2
-        },
-        {
-          "ingredient": 1145,
-          "amount": 4
-        },
-        {
-          "ingredient": 1002030,
-          "amount": 4
-        },
-        {
-          "ingredient": 12061,
-          "amount": 2
-        },
-        {
-          "ingredient": 19335,
-          "amount": 4
-        },
-        {
-          "ingredient": 15152,
-          "amount": 3
-        },
-        {
-          "ingredient": 9003,
-          "amount": 2
-        },
-        {
-          "ingredient": 18372,
-          "amount": 3
-        },
-        {
-          "ingredient": 2027,
-          "amount": 2
-        }
-      ]
-    }
-    user = new User(userInfo);
-    pantry = new Pantry(user);
+    ingredients = [
+      {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 18372,
+        "amount": 4
+      },
+      {
+        "ingredient": 20081,
+        "amount": 2
+      }
+    ]
+    pantryFull = new Pantry(ingredients);
+    pantryEmpty = new Pantry();
     recipeInfo1 = {
       "id": 595736,
       "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
@@ -183,49 +43,7 @@ describe('Pantry', function() {
             "amount": 0.5,
             "unit": "tsp"
           }
-        },
-        {
-          "id": 1123,
-          "quantity": {
-            "amount": 1,
-            "unit": "large"
-          }
         }
-      ],
-      "instructions": [
-        {
-          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
-          "number": 1
-        },
-        {
-          "instruction": "Add egg and vanilla and mix until combined.",
-          "number": 2
-        },
-        {
-          "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
-          "number": 3
-        },
-        {
-          "instruction": "Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.",
-          "number": 4
-        },
-        {
-          "instruction": "Bake for 9 to 10 minutes, or until you see the edges start to brown.",
-          "number": 5
-        },
-        {
-          "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
-          "number": 6
-        }
-      ],
-      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
-      "tags": [
-        "antipasti",
-        "starter",
-        "snack",
-        "appetizer",
-        "antipasto",
-        "hor d'oeuvre"
       ]
     };
     recipeInfo2 = {
@@ -235,29 +53,22 @@ describe('Pantry', function() {
         {
           "id": 1009016,
           "quantity": {
-            "amount": 1.5,
+            "amount": 10.5,
             "unit": "cups"
           }
         },
         {
           "id": 9003,
           "quantity": {
-            "amount": 2
+            "amount": 20
           }
         }
       ],
       "instructions": [
         {
           "instruction": "Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!",
-          "number": 1
+          "number": 10
         }
-      ],
-      "name": "Maple Dijon Apple Cider Grilled Pork Chops",
-      "tags": [
-        "lunch",
-        "main course",
-        "main dish",
-        "dinner"
       ]
     },
     recipe1 = new Recipe(recipeInfo1);
@@ -269,18 +80,32 @@ describe('Pantry', function() {
   });
 
   it('should instantiate a new pantry', function () {
-    expect(pantry).to.be.an.instanceOf(Pantry);
+    expect(pantryFull).to.be.an.instanceOf(Pantry);
+    expect(pantryEmpty).to.be.an.instanceOf(Pantry);
   });
 
-  it("should be initialized by the user's pantry", function() {
-    expect(pantry.pantry.length).to.deep.equal(user.pantry.length);
+  it('can have no ingredients by default', function () {
+    expect(pantryEmpty.ingredients).to.deep.equal([]);
   });
 
-  it.only('should determine if I have enough ingredients for a meal', function() {
-    expect(pantry.determineSufficientIngredients(recipe1)).to.equal(true);
-    expect(pantry.determineSufficientIngredients(recipe2).length).to.deep.equal(1);
+  it('can be instantiated with ingredients', function() {
+    expect(pantryFull.ingredients).to.deep.equal(ingredients);
+  });
 
-  })
+  it.only('has a helper method to determine if the pantry has sufficient ingredients', function() {
+
+  });
+
+  it('should determine if pantry has enough ingredients for meal', function() {
+    expect(pantryFull.checkPantry(recipe1)).to.equal(true);
+    expect(pantryFull.checkPantry(recipe2)).to.equal(false);
+    expect(pantryEmpty.checkPantry(recipe1)).to.equal(false);
+  });
+
+  // it('should determine if I have enough ingredients for a meal', function () {
+  //   expect(pantry.checkPantry(recipe1)).to.equal(true);
+  //   expect(pantry.checkPantry(recipe2)).to.equal(false);
+  // })
 
 
 });
